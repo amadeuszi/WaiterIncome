8009c9e848fcfb4f302625089fcbc5cb5f69fa2e
Amadeusz Iwanowski <amadeusz.iwanowski@gmail.com>
2019-12-20 05:40:58 +0100

    Add dumb component to check if project works

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {DumbComponent} from "./DumbComponent";
 
 const App: React.FC = () => {
   return (
@@ -8,7 +9,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. <DumbComponent />
         </p>
         <a
           className="App-link"
---
+++ src/DumbComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react'; // we need this to make JSX compile
+
+export const DumbComponent = () => {
+
+    return <div>
+        <p>dumb component</p>
+    </div>
+};
